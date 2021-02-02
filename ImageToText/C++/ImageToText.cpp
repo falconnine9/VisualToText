33648f2df6843a0abff3644ce8deb30904828fc9
falconnine9 <63831264+falconnine9@users.noreply.github.com>
2021-02-01 17:23:35 -0800

    Optimizations

--- ImageToText/C++/ImageToText.cpp
+++ ImageToText/C++/ImageToText.cpp
@@ -9,22 +9,28 @@
 
 int imageToText(std::string& filename, unsigned int* scale, std::ofstream& output) {
 	cv::Mat img;
-	img = cv::imread(filename);
+	img = cv::imread(filename, 0);
 
 	if (img.empty()) {
 		return -1;
 	}
 
-	cv::cvtColor(img, img, cv::COLOR_BGR2GRAY, 1);
-	cv::resize(img, img, cv::Size(std::round(img.rows / scale[0]), std::round(img.cols / scale[1])), cv::INTER_NEAREST);
+	cv::resize(img, img, cv::Size(img.rows / scale[0], img.cols / scale[1]), cv::INTER_NEAREST);
 
+	std::string output_str;
+	int pos = 0;
+	output_str.resize(img.rows * (img.cols + 1));
 	for (int x = 0; x < img.rows; x++) {
 		for (int y = 0; y < img.cols; y++) {
 			char pixel = brightness[(img.at<uchar>(x, y) * 64) >> 8];
-			output << pixel;
+			output_str[pos] = pixel;
+			pos++;
 		}
-		output << '\n';
+		output_str[pos] = '\n';
+		pos++;
 	}
+
+	output << output_str;
 
 	return -1;
 }
